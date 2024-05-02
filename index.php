3587f31c232dd2790b9d94e314ac90e87e678309
halayumu <ookamiayumuwww@gmail.com>
2024-05-02 15:28:50 +0900

    フォームの作成 #2　DBに必要な入力フォームを作成

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
-<?php
-echo "hello";
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>DB登録</title>
+</head>
+
+<body>
+	<header>
+		<h1>登録画面</h1>
+	</header>
+
+	<main>
+		<form action="" method="post">
+
+			<label for="name">名前</label><br>
+			<input type="text" id="name" name="name"><br>
+
+			<label for="age">年齢</label><br>
+			<input type="text" id="age" name="age"><br>
+
+			<input type="submit" value="送信">
+		</form>
+	</main>
+
+</body>
+
+</html>
