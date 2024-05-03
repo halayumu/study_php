f5084034c7a78e961d167fac1e0e4e1b75081e6a
halayumu <ookamiayumuwww@gmail.com>
2024-05-03 17:39:24 +0900

    フォームの入力データを登録 #6　接続処理の改善のため一度削除

--- listUser.php
+++ listUser.php
@@ -1,16 +1,1 @@
 <?php
-/// 接続設定
-$servername = "localhost"; // サーバーのアドレス
-$username = "user"; // ユーザー名
-$password = ""; // パスワード
-$database = "test"; // データベース名
-
-/// DB接続
-$mysqli = new mysqli($servername, $username, $password, $database);
-
-/// 接続チェック
-if ($mysqli->connect_errno) {
-  die("接続失敗: " . $mysqli->connect_error); //実行停止
-} else {
-  echo "接続成功";
-}
