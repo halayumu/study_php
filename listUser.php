c8b47adb88c41d21389bf618906885a04c08b4ee
halayumu <ookamiayumuwww@gmail.com>
2024-05-04 16:29:25 +0900

    userテーブルのレコードを全て取得してくる #8　レコード全件メソッドの表示処理

--- listUser.php
+++ listUser.php
@@ -1,1 +1,39 @@
 <?php
+require_once "MySqlOperation.php";
+
+/// DB接続
+$mySqlOperation = new MySqlOperation;
+$mySqlOperation->connect();
+
+/// レコード全件取得
+$allUser = $mySqlOperation->allUserTable();
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>一覧表示</title>
+</head>
+
+<body>
+  <table border="1">
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Age</th>
+    </tr>
+    <?php foreach ($allUser as $user) : ?>
+      <tr>
+        <td><?= $user['id']; ?></td>
+        <td><?= $user['name']; ?></td>
+        <td><?= $user['age']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
