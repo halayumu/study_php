c4d280473844e17277c51b32b8227c5b4d5d4136
halayumu <ookamiayumuwww@gmail.com>
2024-05-09 17:48:39 +0900

    ページングの実装 #10　ページングの実装と画面の表示数制御の処理

--- listUser.php
+++ listUser.php
@@ -8,6 +8,24 @@ $mySqlOperation->connect();
 /// レコード全件取得
 $allUser = $mySqlOperation->allUserTable();
 
+/// 表示件数処理
+define('MAX', '3');
+
+$count = count($allUser);
+
+if (!isset($_GET['pageId'])) {
+  $now = 1;
+} else {
+  $now = $_GET['pageId'];
+  var_dump($now);
+}
+
+$startNo = ($now - 1) * MAX; // 配列の何番目から取得すればよいか
+$dispData = array_slice($allUser, $startNo, MAX, true); // 配列の何番目から何番目(MAX)まで切り取る
+
+/// ページングの数を設定する処理
+$maxPage = ceil($count / MAX);
+
 ?>
 
 <!DOCTYPE html>
@@ -26,7 +44,7 @@ $allUser = $mySqlOperation->allUserTable();
       <th>Name</th>
       <th>Age</th>
     </tr>
-    <?php foreach ($allUser as $user) : ?>
+    <?php foreach ($dispData as $user) : ?>
       <tr>
         <td><?= $user['id']; ?></td>
         <td><?= $user['name']; ?></td>
@@ -34,6 +52,15 @@ $allUser = $mySqlOperation->allUserTable();
       </tr>
     <?php endforeach; ?>
   </table>
+
+  <?php for ($i = 1; $i <= $maxPage; $i++) : ?>
+    <?php if ($i == $now) : ?>
+      <p><?= $now . ' ' ?></p>
+    <?php else : ?>
+      <p><a href="./listUser.php?pageId=<?= $i ?>"><?= $i ?></a>　</p>
+    <?php endif; ?>
+  <?php endfor; ?>
+
 </body>
 
 </html>
