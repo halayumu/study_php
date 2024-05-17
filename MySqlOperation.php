4ccb9c677c31ad4896579fbca2b36a0e6786baf3
halayumu <ookamiayumuwww@gmail.com>
2024-05-17 16:22:05 +0900

    画像のリサイズ #12　画像のリサイズ処理メソッドの作成

--- MySqlOperation.php
+++ MySqlOperation.php
@@ -10,8 +10,8 @@ class MySqlOperation
 
     $this->mysqli = new mysqli($servername, $username, $password, $database);
 
-    if ($mysqli->connect_errno) {
-      die("接続失敗: " . $mysqli->connect_error); //実行停止
+    if ($this->mysqli->connect_errno) {
+      die("接続失敗: " . $this->mysqli->connect_error); //実行停止
     }
   }
 
@@ -41,5 +41,40 @@ class MySqlOperation
     }
 
     return $allRecords;
+  }
+
+  /// 画像のリサイズ
+  public function resizeImageToSquare($srcImagePath, $newSize = 300)
+  {
+    // 元の画像サイズを取得
+    list($width, $height) = getimagesize($srcImagePath);
+    $srcImage = imagecreatefromjpeg($srcImagePath);
+
+    // 新しい正方形のキャンバスを作成
+    $dstImage = imagecreatetruecolor($newSize, $newSize);
+    $white = imagecolorallocate($dstImage, 255, 255, 255);
+    imagefill($dstImage, 0, 0, $white);
+
+    // 画像の縦横比を保持してリサイズ
+    if ($width > $height) {
+      $newHeight = $newSize;
+      $newWidth = intval($width * ($newSize / $height));
+      $srcX = intval(($newWidth - $newSize) / 2);
+      $srcY = 0;
+    } else {
+      $newWidth = $newSize;
+      $newHeight = intval($height * ($newSize / $width));
+      $srcX = 0;
+      $srcY = intval(($newHeight - $newSize) / 2);
+    }
+
+    // 元の画像を新しいキャンバスにコピーしてリサイズ
+    imagecopyresampled($dstImage, $srcImage, 0, 0, $srcX, $srcY, $newWidth, $newHeight, $width, $height);
+
+    // 画像表示をサーバ側に教える
+    header("Content-Type: image/jpeg");
+
+    // 新しい画像をJPEG形式で保存（品質指定）
+    imagejpeg($dstImage);
   }
 }
