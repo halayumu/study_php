086a9bc141db12ab973ddffe0d35d937ea7f9746
halayumu <ookamiayumuwww@gmail.com>
2024-05-17 16:24:47 +0900

    画像のリサイズ #12　コードフォーマット

--- index.php
+++ index.php
@@ -2,29 +2,31 @@
 <html lang="ja">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>DB登録</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DB登録</title>
 </head>
 
 <body>
-	<header>
-		<h1>登録画面</h1>
-	</header>
+  <header>
+    <h1>登録画面</h1>
+  </header>
 
-	<main>
-		<form action="./AddUser.php" method="post">
-			<label for="name">名前</label><br>
-			<input type="text" id="name" name="name"><br>
+  <main>
+    <form action="./AddUser.php" method="post" enctype="multipart/form-data">
+      <label for="name">名前</label><br>
+      <input type="text" id="name" name="name"><br>
 
-			<label for="age">年齢</label><br>
-			<input type="text" id="age" name="age"><br>
+      <label for="age">年齢</label><br>
+      <input type="text" id="age" name="age"><br>
 
-			<input type="submit" value="送信">
-		</form>
+      <input type="file" name="image"><br>
 
-		<input type="button" onclick="location.href='./listUser.php'" value=" 一覧画面">
-	</main>
+      <input type="submit" value="送信">
+    </form>
+
+    <input type="button" onclick="location.href='./listUser.php'" value=" 一覧画面">
+  </main>
 
 </body>
 
